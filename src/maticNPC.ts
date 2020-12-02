8ee71de181fb432ce53384532a6b7974713e4e29
Alexey Knyazev <aleks.kn@gmail.com>
2020-12-03 00:25:04 +0300

    matic

--- src/maticNPC.ts
+++ src/maticNPC.ts
@@ -13,15 +13,91 @@ export class MaticNPC extends NPCBase {
             text: 'Matic Network brings massive scale to Ethereum using an adapted version of Plasma with PoS based side chains. High throughput and free transactions.',
             triggeredByNext: () => {
                 const balance = this.getMaticBalance()
-                // if ( > 0) {
-                //     log('matic')
-                // }
             },
             isEndOfDialog: true,
         },
         {
+            name: '1_no_mana',
+            text: 'You do not have enough MANA!',
+        },
+        {
+            text: 'You need to buy more MANA!',
+            isQuestion: true,
+            buttons: [
+                {label: `Ok`, goToDialog: 'end'},
+                {label: `Close`, goToDialog: 'end'},
+                {
+                    label: `Kyberswap`, goToDialog: 'end',
+                    triggeredActions: () => {
+                        openExternalURL('https://kyberswap.com/swap/eth-mana');
+                    }
+                },
+                {
+                    label: `Uniswap`, goToDialog: 'end',
+                    triggeredActions: () => {
+                        openExternalURL('https://app.uniswap.org/#/swap');
+                    }
+                },
+            ]
+        },
+        {
             name: '1_balance',
             text: ''
+        },
+        {
+            name: 'top_up_intro',
+            text: `I can help to top up your Matic balance.`,
+        },
+        {
+            text: `At any time you can withdraw money back or spend it on Lottery here, or Casino in DCL, or QuizZone, or Wearables (in the future), etc.`,
+        },
+        {
+            name: 'top_up_start',
+            text: `Do you want to top up your Matic MANA balance?`,
+            isQuestion: true,
+            buttons: [
+                {label: `Yes`, goToDialog: 'top_up_input',
+                    triggeredActions: () => {
+                        this.showInputText()
+                    }
+                },
+                {label: `Later`, goToDialog: 'end'},
+                {label: `Matic Wallet`, goToDialog: 'end',
+                    triggeredActions: () => {
+                        openExternalURL('https://wallet.matic.network/')
+                    }
+                },
+            ]
+        },
+        {
+            name: 'top_up_input',
+            text: `Enter the amount of MANA:`,
+            offsetY: 50,
+            triggeredByNext: () => {
+                this.hideInputText()
+            },
+            isQuestion: true,
+            buttons: [
+                {
+                    label: `Send`, goToDialog: 'top_up_send',
+                    triggeredActions: () => {
+                        this.textSubmit()
+                    }
+                },
+            ]
+        },
+        {
+            name: 'top_up_send',
+            text: `You will have to sign TWO transactions`,
+        },
+        {
+            text: `After main MANA is withdrawn, matic MANA will appear shortly`,
+            isEndOfDialog: true,
+        },
+        {
+            name: 'top_up_complete',
+            text: `Complete! The whole procedure takes 5-10 minutes`,
+            isEndOfDialog: true,
         },
         {
             name: 'end',
@@ -36,6 +112,7 @@ export class MaticNPC extends NPCBase {
     ]
     fillInCanvas: UIInputText
     submittedText: string = ''
+    network: string = 'mainnet'
     active = true
     state : number | string = 0
     private balance: { l1: number; l2: number };
@@ -45,6 +122,9 @@ export class MaticNPC extends NPCBase {
             position,
             "models/robot.glb")
         this.dlgScript = this.maticDlg
+
+        //for debug
+        this.network = 'goerli'
     }
 
     updateBalanceText() {
@@ -53,17 +133,36 @@ export class MaticNPC extends NPCBase {
 
     private getMaticBalance() {
         executeTask(async () => {
-            this.balance = await layerTwo.matic.balance()
-            log(this.balance)
-            this.updateBalanceText()
-            if (this.balance.l2 >= 10) {
-                this.talk(this.dlgScript, '1_balance')
-            }
-            if (this.balance.l1 < 10) {
-
-            }
-            return this.balance
+            await layerTwo.matic.balance(null, this.network).then((res) =>
+            {
+                this.balance = res
+                log(this.balance)
+                this.updateBalanceText()
+                if (this.balance.l1 < 10 && this.balance.l2 < 10) {
+                    this.talk(this.dlgScript, '1_no_mana')
+                } else {
+                    this.talk(this.dlgScript, '1_balance')
+                }
+            }).catch((e) =>
+            {
+                this.showError(e.toString())
+            })
         })
-        return null
+    }
+
+    textSubmit() {
+        super.textSubmit();
+        executeTask(async () => {
+            const amount = parseFloat(this.submittedText)
+            const res = await layerTwo.matic.depositMana(amount, this.network).then((res) =>
+            {
+                log(res)
+                this.talk(this.dlgScript, 'top_up_complete')
+            }).catch((e) =>
+            {
+                this.showError(e.toString())
+            })
+            log(res)
+        })
     }
 }
