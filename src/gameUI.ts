53df1b6f7acfa22a5c004b1dc810b502b6503d04
Demyan <demyan.golyshev@gmail.com>
2020-12-05 12:55:58 +0300

    fix

--- src/gameUI.ts
+++ src/gameUI.ts
@@ -4,10 +4,10 @@ import {BarStyles} from '../node_modules/@dcl/ui-utils/utils/types'
 export class GameUI {
     color = Color4.Green()
     level = new ui.UICounter(0, -100, 150, this.color)
-    levelLabel = new ui.CornerLabel('Wave:', -170, 150, this.color)
+    levelLabel = new ui.CornerLabel('WAVE:', -170, 150, this.color)
 
     score = new ui.UICounter(0, -100, 100, this.color)
-    scoreLabel = new ui.CornerLabel('Kills:', -170, 100, this.color)
+    scoreLabel = new ui.CornerLabel('DESTROYED:', -170, 100, this.color)
 
     // health = new ui.UIBar(1, -30, 60, Color4.Red(), BarStyles.ROUNDSILVER, 1)
     // healthLabel = new ui.CornerLabel('Health:', -170, 55, this.color)
